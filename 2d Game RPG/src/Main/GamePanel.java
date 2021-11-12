133716d132912a3ab76b12498c7e7c79a22929bb
erickgonzalez23179 <70284978+erickgonzalez23179@users.noreply.github.com>
2021-11-12 10:58:03 -0500

    Deleted duplicate variables

--- 2d Game RPG/src/Main/GamePanel.java
+++ 2d Game RPG/src/Main/GamePanel.java
@@ -31,11 +31,7 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	Thread gameThread;
 	Player player = new Player(this,keyH);
-	
-	// set position
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
+		
 	
 	public GamePanel () {
 		
