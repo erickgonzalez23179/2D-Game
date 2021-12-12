d2f32e7e5c161db689ae434cd18ba2855b884787
erickgonzalez23179 <70284978+erickgonzalez23179@users.noreply.github.com>
2021-12-11 22:14:01 -0500

    added sounds that are copyrighted and key and door functions

---
+++ 2d Game RPG/src/object/Boots.java
@@ -0,0 +1,20 @@
+package object;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Boots extends SuperObject{
+	
+	public Boots() {
+		name = "Boots";
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream("/objects/boots.png"));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+}
